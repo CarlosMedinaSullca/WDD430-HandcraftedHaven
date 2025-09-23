506d74467fac6ae6dacebcc89aabd8513874e9e3
Ivan Leffalle <ivanmleffalle@gmail.com>
2025-09-23 19:02:07 -0300

    week 4 - ui updates

--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -1,36 +1,37 @@
-import { Search, ShoppingCart, User, Menu, X } from "lucide-react";
+import { Search, ShoppingCart, User } from "lucide-react";
 import { SearchBar } from "../ui/searchBar";
 import { MobileMenu } from "../ui/menu";
 import { Logo } from "../ui/logo";
+
 export function NavBar() {
   return (
-    <nav className="bg-white shadow-md border-b border-gray-200">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+    <nav className="bg-white w-full shadow-md border-b border-gray-200">
+      <div className="sm:px-6 lg:px-8">
+        <div className="flex flex-col md:flex-row items-center justify-center h-16 gap-2 md:gap-8">
           {/* Logo */}
           <Logo />
-          {/* Search Bar - Hidden on mobile */}
-          <SearchBar />
+
+          {/* Search Bar */}
+          <div className="w-full md:w-auto">
+            <SearchBar />
+          </div>
+
           {/* Desktop Navigation */}
-          <div className="hidden md:flex items-center space-x-4">
+          <div className="flex items-center space-x-4">
             <button className="flex items-center space-x-1 px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-50 transition-colors duration-200">
               <ShoppingCart className="h-5 w-5" />
-              <span>Cart</span>
-              <span className="bg-blue-600 text-white text-xs rounded-full px-2 py-1 ml-1">
-                0
-              </span>
             </button>
 
             <button className="flex items-center space-x-1 px-3 py-2 rounded-2xl text-sm font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-50 transition-colors duration-200">
-              <User className="h-5 w-5" />
-              <span>Login</span>
+              <span>Login/Signup</span>
             </button>
 
-            <button className="flex items-center space-x-1 px-3 py-2 rounded-2xl text-sm font-medium bg-blue-600 text-white hover:bg-blue-700 transition-colors duration-200">
+            <button className="flex items-center space-x-1 px-3 py-2 rounded-2xl text-sm font-medium bg-[#16796F] text-white hover:bg-blue-700 transition-colors duration-200">
               <User className="h-5 w-5" />
-              <span>Profile</span>
             </button>
           </div>
+
+          {/* Mobile Menu */}
           <MobileMenu />
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+import { Banner } from "./components/banner";
+import Carrousel from "./components/Carrousel";
 import { NavBar } from "./components/navBar";
+
 export default function Home() {
+  const categoriesMock = [
+    { name: "Example 1", src: "/categories/categorie1.png" },
+    { name: "Example 2", src: "/categories/categorie2.png" },
+    { name: "Example 3", src: "/categories/categorie3.png" },
+  ];
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <header>
-          <NavBar />
-        </header>
+    <div className="font-sans min-h-screen flex flex-col items-center">
+      <header className="sticky top-0 z-50 w-full bg-white shadow">
+        <NavBar />
+      </header>
+
+      <main className="flex flex-col gap-8 max-w-7xl items-center p-8 pt-8">
+        <Banner />
+        <Carrousel list={categoriesMock} />
       </main>
     </div>
   );
