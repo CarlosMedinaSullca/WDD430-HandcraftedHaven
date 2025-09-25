cc165c87d192497b38ba50a352b9fc92face7ae1
CarlosMedinaSullca <carlos.medina.sullca@gmail.com>
2025-09-25 18:05:25 -0400

    change to test vercel deployment

--- src/app/ui/searchBar.tsx
+++ src/app/ui/searchBar.tsx
@@ -23,7 +23,7 @@ export function SearchBar() {
           <input
             type="search"
             value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value)} 
             onKeyDown={(e) => e.key === "Enter" && handleSearch()}
             placeholder="Search products..."
             className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-2xl leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
