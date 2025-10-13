22673b279fdefb774369f8878cb3dd43260d9234
munyaradzichiondegwa <bchiondegwa@byupathway.edu>
2025-10-13 03:00:34 +0200

    new changes

--- src/app/my-products/page.tsx
+++ src/app/my-products/page.tsx
@@ -2,8 +2,7 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import MyProductsClient from "./MyProductsClient";
-
+import MyProductsClient from "./components/MyProductsClient";
 export default async function MyProductsPage() {
   const session = await getServerSession(authOptions);
   if (!session || session.user?.role !== "seller") {
